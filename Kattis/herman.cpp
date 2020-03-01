05ffbe4f49639010c0dcc1b2050884f789274f8e
Ian Yong <ianyongyc@gmail.com>
2020-03-01 13:19:21 +0800

    Add Kattis - Herman

---
+++ Kattis/herman.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int R;
+
+int main() {
+    scanf("%d", &R);
+    printf("%.6f\n", (M_PI * R * R));
+    printf("%.6f\n", ((2.0 * R) * (2 * R) / 2));
+}
