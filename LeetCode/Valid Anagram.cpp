5a7c197f33d90d044a5e60b73efc657d4821cec7
Ian Yong <ianyongyc@gmail.com>
2021-08-28 23:09:25 +0800

    Add LeetCode - Valid Anagram

---
+++ LeetCode/Valid Anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int letters[26] = {};
+        for (int i = 0; i < s.length(); i++) {
+            letters[s[i] - 'a']++;
+            letters[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (letters[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
