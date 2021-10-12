8bd28ae60de09a4fa5b174a013f5294b3a0d4514
Ian Yong <ianyongyc@gmail.com>
2021-10-12 21:30:03 +0800

    Add LeetCode 208 - Implement Trie (Prefix Tree)

---
+++ LeetCode/208 - Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,60 @@
+class Trie {
+private:
+    class TrieNode {
+        public:
+            bool isWord;
+            unordered_map<char, TrieNode*> children;
+
+            TrieNode() {
+                isWord = false;
+            }
+    };
+
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                node->children[c] = new TrieNode();
+            }
+            node = node->children[c];
+        }
+        node->isWord = true;
+    }
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            if (node->children.find(c) == node->children.end()) {
+                return false;
+            }
+            node = node->children[c];
+        }
+        return node->isWord;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char c : prefix) {
+            if (node->children.find(c) == node->children.end()) {
+                return false;
+            }
+            node = node->children[c];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
