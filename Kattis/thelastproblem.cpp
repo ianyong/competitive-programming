4fff29dc45d10e5a8283cc5101c2f28ba14a25c7
Ian Yong <ianyongyc@gmail.com>
2022-08-18 01:13:44 +0800

    Add Kattis - The Last Problem

---
+++ Kattis/thelastproblem.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+using namespace std;
+
+#define MAX_S_LENGTH 50
+
+char S[MAX_S_LENGTH + 1];
+
+int main() {
+    scanf("%[^\n]", S);
+    printf("Thank you, %s, and farewell!\n", S);
+}
