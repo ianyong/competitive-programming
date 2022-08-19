03accf1dc2b63e9b5cf3bf3920c8d770c5fa39ea
Ian Yong <ianyongyc@gmail.com>
2022-08-19 16:11:51 +0800

    Add UVa 11172 - Relational Operator

---
+++ UVa/Volume 111/11172 - Relational Operator.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+
+int t, a, b;
+
+int main() {
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d %d", &a, &b);
+        if (a < b) {
+            printf("<\n");
+        } else if (a > b) {
+            printf(">\n");
+        } else {
+            printf("=\n");
+        }
+    }
+}
